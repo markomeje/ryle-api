e3fc63cb9375aa3613927b990e0d95bac6eee17c
markomeje <markomejeonline@gmail.com>
2025-12-19 03:33:23 +0100

    first commit

---
+++ src/main/java/com/ryle/RyleApplication.java
@@ -0,0 +1,14 @@
+package com.ryle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyleApplication.class, args);
+		System.out.println("Ryle Application Started");
+	}
+
+}
